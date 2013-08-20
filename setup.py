c599476e04982fcf7e983f37615b9a0946d196ab
Kaloyan M. Penev <kpenev@princeton.edu>
2013-08-20 14:16:47 +0000

    trunk/setup.py: 	* More complete meta information provided for the OrbitSolver python 	  module trunk/MANIFEST.in: 	* Adds all files missed by the default MANIFEST generation for the 	  OrbitSolver python module

--- setup.py
+++ setup.py
@@ -21,6 +21,25 @@ YREC_tracks=glob(join_paths('YREC', '*.track'))
 setup(name='OrbitSolver',
       version='1.0',
       author='Kaloyan Penev',
+      author_email='kpenev@gmail.com',
+      description='Calculate the orbital evolution of a planet-star system.',
+      long_description=
+      """ For a system consisting of a star and a single planet in a circular
+      orbit aligned with the stellar rotation, calculate the evolution of the
+      semimajor axis and the rotation of the star. If the star has a mass
+      below 1.1 solar masses, the rotation of the radiative core and the
+      convective envelope are evolved separately. The following physics is
+      included:
+        * The evolution of the semimajor axis of the orbit due to the tidal
+          dissipation in the star.
+        * The evolution of the angular momentum of the stellar convective
+          envelope by the tidal coupling.
+        * The transfer of angular momentum between the stellar convective and
+          radiative zones.
+        * The effect of the stellar evolution on the tidal dissipation
+          efficiency, and stellar core and envelope spins.
+        * The loss of stellar convective zone angular momentum to a
+          magnetically launched wind. """,
       ext_modules=[Extension('OrbitSolver',
                              ['OrbitSolverModule.cpp']+
                              orbital_evolution_code+
