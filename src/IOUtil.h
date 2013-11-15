f2a881ba01209beb28c7261d60ed1b02b93635b6
Kaloyan M. Penev <kpenev@princeton.edu>
2013-11-15 02:23:48 +0000

    src/debug_mesaio.cpp: 	* A temporary file for debugging the MESA based interpolation. src/IOUtil.h: 	* A place for commonly used I/O related things. src/MESAIO.(h/cpp): 	* Implements interpolation based on MESA stellar evolution tracks.

---
+++ src/IOUtil.h
@@ -0,0 +1,66 @@
+#ifndef __IO_UTIL_H
+#define __IO_UTIL_H
+
+#include <iostream>
+#include <valarray>
+#include <list>
+
+///Exctracts columns from a white space separated stream.
+template<typename COLNUM_STRUCTURE>
+std::valarray< std::list<double> > parse_columns(
+		///The stream which should contain only columns, and the columns
+		///with number listed in column_numbers must be interpretable as
+		///doubles.
+		std::istream &is,
+
+		///The column numbers to extract. Should provide operator[], size and
+		///iterators.
+		const COLNUM_STRUCTURE &column_numbers);
+
+template<class COLNUM_STRUCTURE>
+std::valarray< std::list<double> > parse_columns(std::istream &is,
+		const COLNUM_STRUCTURE &column_numbers)
+{
+	int last_column=*max_element(column_numbers.begin(),
+			column_numbers.end());
+	std::string line;
+	std::valarray< std::list<double> > result(column_numbers.size());
+	unsigned line_number=0;
+	for(std::getline(is, line); !is.eof(); std::getline(is, line)) {
+		++line_number;
+		if(!is.good()) {
+			std::ostringstream msg;
+			msg << "Failed to read line " << line_number
+				<< " of the data section of an input stream in "
+				<< "parse_columns.";
+			throw Error::IO(msg.str());
+		}
+		std::istringstream line_stream(line);
+		for(int column_number=0; column_number<=last_column;
+				++column_number) {
+			std::ostringstream msg;
+			msg << "Failed to parse column " << column_number
+				<< " on line " << line_number << " of the data section of an"
+				"input stream in parse_columns.";
+			if(!line_stream.good()) throw Error::IO(msg.str());
+			typename COLNUM_STRUCTURE::const_iterator
+				colnum_iter=column_numbers.begin();
+			for(size_t result_index=0; result_index<column_numbers.size();
+					++result_index, ++colnum_iter)
+				if(*colnum_iter==column_number) {
+					double value;
+					line_stream >> value;
+					result[result_index].push_back(value);
+					break;
+				}
+			if(colnum_iter==column_numbers.end()) {
+				std::string word;
+				line_stream >> word;
+			}
+			if(!line_stream.good()) throw Error::IO(msg.str());
+		}
+	}
+	return result;
+}
+
+#endif
---
+++ src/debug_mesaio.cpp
@@ -0,0 +1,19 @@
+#include "MESAIO.h"
+
+int main(int, char **argv)
+{
+	try {
+		MESA::Evolution evolution("MESA");
+		const EvolvingStellarQuantity
+			*radius=evolution.interpolate_radius(1.0);
+		std::cout.setf(std::ios_base::scientific);
+		std::cout.precision(16);
+		for(double age=1e-4; age<10; age*=1.01)
+			std::cout << std::setw(25) << age << std::setw(25) << 
+				(*radius)(age) << std::endl;
+		return 0;
+	} catch(Error::General &ex) {
+		std::cout << ex.what() << ": " << ex.get_message() << std::endl;
+		return 1;
+	}
+}
