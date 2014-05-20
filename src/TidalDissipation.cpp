811437856d4f37ad59d00ebab65c7c8c02d777e3
Kaloyan Penev <kpenev@princeton.edu>
2014-05-20 00:26:34 -0400

    Better organization for the TidalDissipation class.

--- src/TidalDissipation.cpp
+++ src/TidalDissipation.cpp
@@ -37,83 +37,93 @@ const double TidalDissipation::__torque_x_minus_coef[]={0.0,
 													    std::sqrt(1.5),
 													    1.0};
 
-void DissipatingBody::fill_Umm(double inclination, 
-		std::valarray< std::valarray<double> > &Umm)
+void DissipatingBody::fill_Umm(double inclination, bool deriv)
 {
 	double c=std::cos(inclination), s=std::sin(inclination),
-		   s2=std::pow(s, 2), sc=s*c;
+		   s2=std::pow(s, 2), sc=s*c, cp1=c+1.0, cm1=c-1.0;
 
-	Umm[0][0]=__Umm_coef[0][0]*std::pow(1.0+c, 2);
-	Umm[1][0]=__Umm_coef[1][0]*(s+sc);
-	Umm[2][0]=__Umm_coef[2][0]*s2;
-	Umm[3][0]=__Umm_coef[3][0]*(s-sc);
-	Umm[4][0]=__Umm_coef[4][0]*std::pow(1.0-c, 2);
+	__Umm[0][0]=__Umm_coef[0][0]*(deriv ? -2.0*s*cp1 : std::pow(cp1, 2));
+	__Umm[1][0]=__Umm_coef[1][0]*(deriv ? cp1+2.0*s2 : s*cp1);
+	__Umm[2][0]=__Umm_coef[2][0]*(deriv ? 2.0*sc : s2);
+	__Umm[3][0]=-__Umm_coef[3][0]*(deriv ? c*cm1-s2 : s*cm1);
+	__Umm[4][0]=__Umm_coef[4][0]*(deriv ? -2.0*s*cm1 ? std::pow(cm1, 2));
 
-	Umm[0][1]=__Umm_coef[0][1]*s2;
-	Umm[1][1]=__Umm_coef[1][1]*sc;
-	Umm[2][1]=__Umm_coef[2][1]*(2.0-3.0*s2);
-	Umm[3][1]=__Umm_coef[3][1]*sc;
-	Umm[4][1]=__Umm_coef[4][1]*s2;
+	__Umm[0][1]=__Umm_coef[0][1]*(deriv ? 2.0*sc : s2);
+	__Umm[1][1]=__Umm_coef[1][1]*(deriv ? 1.0-2.0*s2 : sc);
+	__Umm[2][1]=__Umm_coef[2][1]*(deriv ? -6.0sc : 2.0-3.0*s2);
+	__Umm[3][1]=__Umm_coef[3][1]*(deriv ? 1.0-2.0*s2 : sc);
+	__Umm[4][1]=__Umm_coef[4][1]*(deriv ? 2.0*sc : s2);
 
-	Umm[0][2]=__Umm_coef[0][2]*std::pow(1.0-c, 2);
-	Umm[1][2]=__Umm_coef[1][2]*(s-sc);
-	Umm[2][2]=__Umm_coef[2][2]*s2;
-	Umm[3][2]=__Umm_coef[3][2]*(s+sc);
-	Umm[4][2]=__Umm_coef[4][2]*std::pow(1.0+c, 2);
+	__Umm[0][2]=__Umm_coef[0][2]*(deriv ? -2.0*cm1*s : std::pow(cm1, 2));
+	__Umm[1][2]=-__Umm_coef[1][2]*(deriv ? c*cm1-s2 : s*cm1);
+	__Umm[2][2]=__Umm_coef[2][2]*(deriv ? 2.0*sc : s2);
+	__Umm[3][2]=__Umm_coef[3][2]*(deriv ? c*cp1-s2 : s*cp1);
+	__Umm[4][2]=__Umm_coef[4][2]*(deriv ? -2.0*cp1*s: std::pow(cp1, 2));
+}
+
+void TidalDissipation::calculate_torque_power(DissipatingBody &body,
+		short forcing_sign, double &power, double &torque_x,
+		double &torque_y)
+{
+	double spin_frequency=(forcing_sign ? __orbital_frequency :
+				   		   body.spin_frequency()),
+	torque_x=0, torque_z=0, power=0;
+	for(int m=-2; m<=2; ++m) {
+		double m_spin_freq=m*spin_frequency;
+		for(int mp=-2; mp<=2; mp+=2) {
+			double Umm_squared=std::pow(Umm[m][mp], 2),
+				   mod_phase_lag=body.modified_phase_lag(m, 
+						   mp*__orbital_frequency-m_spin_freq, spin_frequency,
+						   forcing_sign),
+			torque_z+=Umm_squared*m*mod_phase_lag;
+			power+=Umm_squared*mp*mod_phase_lag;
+			torque_x+=Umm[m][mp]*(
+					(mp>-2 ? __torque_x_minus_coef[mp]*Umm[m-1][mp] : 0)+
+					(mp<2 ? __torque_x_plus_coef[mp]*Umm[m+1][mp] : 0));
+		}
+	}
 }
 
 TidalDissipation::TidalDissipation(DissipatingBody body1, 
 		DissipatingBody body2, double semimajor, double eccentricity, 
-		short forcing_sign1, short forcing_sign2)
-{
-	std::valarray< std::valarray<double> > Umm1(std::valarray<double>(3), 5),
-										   Umm2(std::valarray<double>(3), 5);
-	fill_Umm(body1.inclination(), Umm1);
-	fill_Umm(body2.inclination(), Umm2);
-	double orbital_frequency=std::sqrt(
+		short forcing_sign1, short forcing_sign2) :
+	__orbital_frequency=std::sqrt(
 			AstroConst::G*
 			(body1.mass()+body2.mass())*AstroConst::solar_mass/
 			std::pow(semimajor*AstroConst::AU, 3))*AstroConst::day,
-		   torque_common=AstroCosnt::G*AstroConst::Msun*
+	__Umm(std::valarray<double>(3), 5);
+	__dissipation_rates(NaN, 2*NUM_DISSIPATION_QUANTITIES)
+{
+	double torque_common=AstroCosnt::G*AstroConst::Msun*
 			   			 std::pow(AstroConst::Rsun, 3)*AstroConst::Gyr*
 						 AstroConst::day/
-						 std::pow(semimajor*AstroConst::AU, 6),
-		   torque1_scale=std::pow(body1.radius(),5)*std::pow(body1.mass(),2)*
-			   			 torque_common,
-		   torque2_scale=std::pow(body2.radius(),5)*std::pow(body2.mass(),2)*
-			   			 torque_common,
-		   spin_frequency1=(forcing_sign1 ? orbital_frequency :
-				   			body1.spin_frequency()),
-		   spin_frequency2=(forcing_sign1 ? orbital_frequency :
-				   			body1.spin_frequency());
-		   __torque_x=0, __torque_z=0, __power=0;
-	for(int m=-2; m<=2; ++m) {
-		double m_orbit_freq=m*orbital_frequency;
-		for(int mp=-2; mp<=2; mp+=2) {
-			double Umm1_squared=std::pow(Umm1[m][mp], 2),
-				   Umm2_squared=std::pow(Umm2[m][mp], 2),
-				   mod_phase_lag1=modified_phase_lag(m, 
-						   mp*orbital_frequency-m_orbit_freq, spin_frequency1,
-						   forcing_sign1),
-				   mod_phase_lag2=modified_phase_lag(m, 
-						   mp*orbital_frequency-m_orbit_freq, spin_frequency2,
-						   forcing_sign2);
-			__torque1_z+=Umm1_squared*m*mod_phase_lag1;
-			__torque2_z+=Umm2_squared*m*mod_phase_lag2;
-			__power1+=Umm1_squared*mp*mod_phase_lag1;
-			__power2+=Umm2_squared*mp*mod_phase_lag2;
-			__torque1_x+=Umm1[m][mp]*(
-					(mp>-2 ? __torque_x_minus_coef[mp]*Umm1[m-1][mp] : 0)+
-					(mp<2 ? __torque_x_plus_coef[mp]*Umm1[m+1][mp] : 0));
-			__torque2_x+=Umm2[m][mp]*(
-					(mp>-2 ? __torque_x_minus_coef[mp]*Umm2[m-1][mp] : 0)+
-					(mp<2 ? __torque_x_plus_coef[mp]*Umm2[m+1][mp] : 0));
-		}
+	DissipatingBody &body=body1;
+	short forcing_sign=forcing_sign1;
+	for(short body_ind=0; body_ind<2; ++body_ind) {
+
+		fill_Umm(body.inclination());
+		calculate_torque_power(body, forcing_sign,
+				__dissipation_rates[2*POWER+body_ind],
+				__dissipation_rates[2*TORQUEX+body_ind], 
+				__dissipation_rates[2*TORQUEZ+body_ind]);
+
+		fill_Umm(body.inclination(), true);
+		calculate_torque_power(body, forcing_sign,
+				__dissipation_rates[2*DPOWER_DINCLINATION+body_ind],
+				__dissipation_rates[2*DTORQUEX_DINCLINATION+body_ind],
+				__dissipation_rates[2*DTORQUEZ_DINCLINATION+body_ind]);
+
+		double torque_scale=std::pow(body.radius(),5)*
+			std::pow(body.mass(),2)*torque_common;
+		__dissipation_rates[2*POWER+body_ind]*=
+			torque_scale*__orbital_frequency;
+		__dissipation_rates[2*DPOWER_DINCLINATION+body_ind]*=
+			torque_scale*__orbital_frequency;
+		__dissipation_rates[2*TORQUEX+body_ind]*=torque_scale;
+		__dissipation_rates[2*DTORQUEX_DINCLINATION+body_ind]*=torque_scale;
+		__dissipation_rates[2*TORQUEZ+body_ind]*=torque_scale;
+		__dissipation_rates[2*DTORQUEZ_DINCLINATION+body_ind]*=torque_scale;
+		body=body2;
+		forcing_sign=forcing_sign2;
 	}
-	power1*=T0*orbital_frequency;
-	power2*=T0*orbital_frequency;
-	torque1_x*=T0*AstroConst::Gyr;
-	torque2_x*=T0*AstroConst::Gyr;
-	torque1_z*=T0*AstroConst::Gyr;
-	torque2_z*=T0*AstroConst::Gyr;
 }
--- src/TidalDissipation.h
+++ src/TidalDissipation.h
@@ -3,7 +3,9 @@
 #include "Common.h"
 #include <cmath>
 
-enum DissipationComponent {POWER, TORQUE_X, TORQUE_Z};
+enum DissipationComponent {POWER, TORQUEX, TORQUEZ,
+	DPOWER_DINCLINATION, DTORQUEX_DINCLINATION, DTORQUEZ_DINCLINATION,
+	NUM_DISSIPATION_QUANTITIES};
 
 ///\brief The rates of change of various quantities due to tidal dissipation.
 class TidalDissipation {
@@ -14,40 +16,62 @@ private:
 	///The first index is m+2 (since m starts from -2) and the second index
 	///is m'/2+1 since the only allowed values are -2, 0 and 1.
 	static const double __Umm1_coef[][],
+
+				 ///\brief \f$\kappa_{m,m'}^+/\kappa_{m,m'}\f$ as a function of
+				 /// \f$m=-2 \ldots 2\f$.
 				 __torque_x_plus_coef[],
+
+				 ///\brief \f$\kappa_{m,m'}^-/\kappa_{m,m'}\f$ as a function of
+				 /// \f$m=-2 \ldots 2\f$.
 				 __torque_x_minus_coef[];
 
-	///\brief The tidal dissipation power in body1 in units of 
-	/// \f$\frac{M_\odot R_\odot^2 rad^2}{day^2\,Gyr}\f$
-	double __power1,
+	///The orbital frequency in rad/day.
+	double __orbital_frequency;
 
-		   ///\brief The tidal dissipation power in body2 in units of 
-		   /// \f$\frac{M_\odot R_\odot^2 rad^2}{day^2\,Gyr}\f$
-		   __power2
+	///The \f$\mathcal{U}_{m,m'}\f$ quantities defined in Lai (2012).
+	std::valarray< std::valarray<double> > __Umm;
 
-		   ///\brief The x tidal dissipation torque of body 1 in units of
-		   /// \f$\frac{M_\odot R_\odot^2 rad}{day\,Gyr}\f$.
-		   __torque1_x,
-
-		   ///\brief The x tidal dissipation torque of body 2 in units of
-		   /// \f$\frac{M_\odot R_\odot^2 rad}{day\,Gyr}\f$.
-		   __torque2_x,
-
-		   ///\brief The z tidal dissipation torque of body 1 in units of
-		   /// \f$\frac{M_\odot R_\odot^2 rad}{day\,Gyr}\f$.
-		   __torque1_z
-		  
-		   ///\brief The z tidal dissipation torque of body 2 in units of
-		   /// \f$\frac{M_\odot R_\odot^2 rad}{day\,Gyr}\f$.
-		   __torque2_z;
+	///\brief Rates of change of the various quantities and derivatives due
+	///to tidal dissipation
+	std::valarray<double> __dissipation_rates;
 
 	///Computes the \f$\mathcal{U}_{m,m'}\f$ values. 
 	void fill_Umm(
 			///The angle between the spin angular momentum of the body and
 			///the orbital angular momentum in radians.
-			double inclination,
+			double inclination
 			
-			std::valarray< std::valarray<double> > &Umm);
+			///Whether to use the derivatives w.r.t. \f$\Theta\f$ instead of
+			///the values
+			bool deriv);
+
+	///\brief Calculates the dimensionless x and y torques and power due to
+	///tidal dissipation.
+	///
+	///The \f$\mathcal{U}_{m,m'}\f$ constants must already be filled.
+	void calculate_torque_power(
+			///The body doing the dissipating.
+			DissipatingBody &body,
+
+			///If this values is not zero, the spin frequency of is /assumed
+			//to approach the orbital frequency from below/above if
+			///forcing_sign is +1/-1 instead, regardless of the currently set
+			///spin frequency of the body. Result is undefined if the spin
+			///frequency is not very close to the orbital frequency and yet
+			///this value is non-zero.
+			short forcing_sign,
+
+			///Set to the tidal dissipation power in the same units as
+			///power_scale.
+			double &power,
+
+			///Set to the tidal dissipation torque in the x direction, in the
+			///same units as torque_scale.
+			double &torque_x,
+
+			///Set to the tidal dissipation torque in the z direction, in the
+			///same units as torque_scale.
+			double &torque_y);
 public:
 	///\brief Calculates the rates of change of various quantities due to
 	///tidal dissipation.
