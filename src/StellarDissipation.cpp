3c861103f342dafb4e5960a53fc2bae33381acd2
Kaloyan Penev <kpenev@princeton.edu>
2014-05-24 14:12:17 -0400

    Fixed a sign inversion in stellar dissipation.

--- src/StellarDissipation.cpp
+++ src/StellarDissipation.cpp
@@ -3,5 +3,5 @@
 double Star::modified_phase_lag(int m,  double forcing_frequency,
 		const SpinOrbitLockInfo &lock, PhaseLag::Derivative derivative) const
 {
-	return (forcing_frequency>0 ? -1 : 1)*__mod_phase_lag;
+	return (forcing_frequency>0 ? 1 : -1)*__mod_phase_lag;
 }
