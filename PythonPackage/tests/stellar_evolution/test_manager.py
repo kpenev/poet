130eae2243e272115431c54775cd5556d93132db
Kaloyan Penev <kpenev@phs4.astro.Princeton.EDU>
2016-12-21 22:32:39 -0500

    Added a test file to see if unexpected exceptions are thrown.

--- PythonPackage/tests/stellar_evolution/test_manager.py
+++ PythonPackage/tests/stellar_evolution/test_manager.py
@@ -7,31 +7,51 @@ sys.path.insert(0, os.path.abspath('../../'))
 from stellar_evolution.manager import StellarEvolutionManager
 
 if __name__ == '__main__' :
-    print('Creating a manager')
     manager = StellarEvolutionManager(
         '../../../stellar_evolution_interpolators'
     )
-    print('Getting default interpolator.')
-    default_interp = manager.get_interpolator()
-    print('Re-getting default interpolator')
-    manager.get_interpolator()
-    print('Done')
+
+    interp = manager.get_interpolator(
+        masses = [1.0],
+        metallicities = [0.0],
+        nodes = dict(RADIUS = 100,
+                     ICONV = 100,
+                     LUM = 100,
+                     IRAD = 100,
+                     MRAD = 100,
+                     RRAD = 100),
+        smoothing = dict(RADIUS = 0.1,
+                         ICONV = 0.2,
+                         LUM = 0.3,
+                         IRAD = 0.4,
+                         MRAD = 0.5,
+                         RRAD = 0.6),
+        new_interp_name = 'test_interp_nonan'
+    )
+    print(interp)
+
+    interp = manager.get_interpolator(
+        nodes = dict(RADIUS = 100,
+                     ICONV = 100,
+                     LUM = 100,
+                     IRAD = 100,
+                     MRAD = 100,
+                     RRAD = 100),
+        smoothing = dict(RADIUS = 0.1,
+                         ICONV = 0.2,
+                         LUM = float('nan'),
+                         IRAD = 0.4,
+                         MRAD = 0.5,
+                         RRAD = 0.6),
+        new_interp_name = 'test_interp_lumnan'
+    )
+    print(interp)
     exit(0)
 
-    manager.get_interpolator(
-        [os.path.join(StellarEvolutionManager.default_track_dir,
-                      'M1.0_Z0.015.csv')],
-        dict(RADIUS = 100,
-             ICONV = 100,
-             LUM = 100,
-             IRAD = 100,
-             MRAD = 100,
-             RRAD = 100),
-        dict(RADIUS = 0.1,
-             ICONV = 0.2,
-             LUM = 0.3,
-             IRAD = 0.4,
-             MRAD = 0.5,
-             RRAD = 0.6),
-        db_session
-    )
+    print(manager.get_by_name('test_interp_nonan'))
+
+    print('Getting default interpolator.')
+    print(manager.get_interpolator(new_interp_name = 'default'))
+    print('Re-getting default interpolator')
+    print(manager.get_interpolator())
+    print('Done')
