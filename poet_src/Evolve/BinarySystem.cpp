e9e7e21e8d69684c76d667e8f8f26c6c2142ed38
Kaloyan Penev <kaloyan.penev@utdallas.edu>
2019-01-11 12:01:08 -0600

    Fixed a bug with locked zone management.

--- poet_src/Evolve/BinarySystem.cpp
+++ poet_src/Evolve/BinarySystem.cpp
@@ -526,14 +526,14 @@ namespace Evolve {
                             1.5
                             *
                             (
-                                body->tidal_power(locked_ind, true)
+                                body->tidal_power(zone_ind, true)
                                 -
-                                body->tidal_power(locked_ind, false)
+                                body->tidal_power(zone_ind, false)
                             )
                             /
                             __orbital_energy
                             /
-                            __semimajor*coef
+                            __semimajor * coef
                         );
 
                         ++locked_ind;
