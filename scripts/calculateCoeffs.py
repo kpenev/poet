14fa6238d4d0593b5c9ff608788c41ebb2056994
Vortebo <64214314+Vortebo@users.noreply.github.com>
2020-09-18 12:03:08 -0500

    Built out pseudocode
    Reconsidered some design choices, moved some parts further along the scale of pseudo vs. code

--- scripts/calculateCoeffs.py
+++ scripts/calculateCoeffs.py
@@ -50,7 +50,7 @@ def t4(x,m,s,e):
 	return exp(i*s*(x-e*sin(x)))*sin(x)/(1-e*cos(u))**4
 	
 # The overall coefficient thing
-def p_MS(x,m,s,e):
+def p_MS(m,s,e):
 	
 	p0s = quad(p_0s,0,2*pi,args=(m,s,e))
 	
@@ -70,28 +70,24 @@ def p_MS(x,m,s,e):
 			term4 = -term4
 		
 	else:
-		#term1 = 0
-		#term2 = 0
-		#term3 = 0
-		#term4 = 0
 		term1=term2=term3=term4 = 0
 		
 	return (1/(2*pi))*(p0s+term1+term2+term3+term4)
 
 # Documentation goes here
 # Gets the specified coefficient of the (Chebyshev?) expansion
-def getCoefficients(equation, [termsToFind], ):
-	for each term to find
-		result[] += findChebyshevExpansion(equation, termsToFind[i]) # There will be some built in thing, I'm sure
-																	 # If not, I guess I'll be defining this function later
-	return result
+# def getCoefficients(equation, [termsToFind], ):
+	# for each term to find
+		# result[] += findChebyshevExpansion(equation, termsToFind[i]) # There will be some built in thing, I'm sure
+																	 #If not, I guess I'll be defining this function later
+	# return result
 
 # Removes junk terms
-def prune(lOC):
-	for each term in lOC
-		if term is greater than 10
-			delete the term from lOC
-	return lOC
+# def prune(lOC):
+	# for each term in lOC
+		# if term is greater than 10
+			# delete the term from lOC
+	# return lOC
 
 # Checks if we're accurate enough
 def checkAccuracy(ListOfCoefficients, equation, [range of e], pms):
@@ -103,10 +99,6 @@ def checkAccuracy(ListOfCoefficients, equation, [range of e], pms):
 	else if there's only one term, problem solved I guess
 	return accuracy
 
-### Run stuff
-#function(0,1)
-
---start execution
 # I'm not sure if I want the user to specify a range of m and s or just a specific p_ms (the latter would need
 # the program to be run individually for each p_ms coefficient)
 def main(m=0, s=1, accuracyGoal, maxCoeffs):
@@ -114,31 +106,36 @@ def main(m=0, s=1, accuracyGoal, maxCoeffs):
 	# Sanity check input arguments
 		# Correct number of them? Correct type? Acceptable values?
 		# If not, complain and exit
-	
-	# Set up the exact equation we're going to use
-		# if m = 0, all terms but first shouldn't be there
-		# if m=+/- 2, those terms should be there
-		# Options:
-			# 1. Variable referring to equation
-				# a. Set some constant that makes the equation be the form I want (possibly slow, might spend time recalculating an integral
-				 # ... only to multiply by zero)
-				# b. Change which equation the variable points to (pointers!)
-			# 2. ??? how does numpy work w/ numerical integrating equations?
 		
+	# Initialize my variables
+	coeffDeg = [0,5] # Current degree(s) of Chebyshev polynomial for which we are finding coefficients
+	listOfCoeff = []
+	
+	# Optional?
 	# Determine range of e we need to deal with (for speed purposes)
 		# As s increases, the point where there's noticeable divergence goes further right
 		# Obviously need full range for getting coeffs, but maybe not for confirming accuracy?
-		
+	
+	# Optional.
 	# Precalculate p_ms(e) itself to compare to? Can I do that? Would be convenient
 	
-	# loop while not accurate enough or number of terms is below maxCoeffs (?)
+	#Calculate some number of data points for 0<=e<=1
+	eList = that
+	
+	# Calculate the value of p_ms for each point on that list
+	vec_pms = vecorize(p_MS) # Allow the bit I defined to take a vector for arguments, hassle-free
+	yList = vec_pms(m,s,eList)
+	
+	for not accurate enough or number of terms is below maxCoeffs (?):
 		
-		# ListOfCoefficients += [ [termsToFind] , getCoefficients(equation, [termsToFind], ) ] # Find however many more coefficients
+		#listOfCoeff += [ [termsToFind] , getCoefficients(equation, [termsToFind], ) ] # Find however many more coefficients
 																							 I think I want lOC to be [ChebNumber,Coef] (2d)
 																							 That way the next step doesn't cause problems
+		listOfCoeff += np.polynomial.chebyshev.chebfit(eList,yList,coeffDeg)
 		
+		# Wait to do this one until you have something running so you can see if it's necessary
 		# ListOfCoefficients = prune(listOfC...) # In Mathematica, sometimes we got terms that exploded, but then ones after that
-											   were fine, so I want to be able to remove problematic terms
+											   #were fine, so I want to be able to remove problematic terms
 		
 		# accuracy = checkAccuracy(ListOfCoefficients, equation, [range of e], pms) # Check to see how accurate we are
 	
