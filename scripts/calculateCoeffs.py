ce30343e7eb40a13ab9a3bfa89efa8935532d783
Vortebo <64214314+Vortebo@users.noreply.github.com>
2020-09-24 17:57:29 -0500

    it goes

--- scripts/calculateCoeffs.py
+++ scripts/calculateCoeffs.py
@@ -11,14 +11,14 @@ import sys, getopt
 import numpy as np
 # import scipy.stats as stats
 import scipy.integrate as integrate
-from integrate import quad
+#from scipy.integrate import quad
 # import matplotlib.pyplot as plt
 # from matplotlib import gridspec
 ####
 
 ### Declare constants here
 #MAX_TERMS = 400
-GRID = 101 # The number of grid points to divide 0<=e<=1 into; should be power of ten plus one
+GRID = 10001 # The number of grid points to divide 0<=e<=1 into; should be power of ten plus one
 
 ### Classes
 #class Classy:
@@ -34,36 +34,36 @@ GRID = 101 # The number of grid points to divide 0<=e<=1 into; should be power o
 
 # Term inside integral for p0s
 def p_0s(x,m,s,e):
-	return exp(i*s*(x-e*sin(x)))/(1-e*cos(u))**2
+	return np.exp(1j*s*(x-e*np.sin(x)))/(1-e*np.cos(x))**2
 
 # The next four terms are the constituent parts
 # of m=+/-2
 def t1(x,m,s,e):
-	return exp(i*s*(x-e*sin(x)))/(1-e*cos(u))**4
+	return np.exp(1j*s*(x-e*np.sin(x)))/(1-e*np.cos(x))**4
 
 def t2(x,m,s,e):
-	return exp(i*s*(x-e*sin(x)))*cos(2*x)/(1-e*cos(u))**4
+	return np.exp(1j*s*(x-e*np.sin(x)))*np.cos(2*x)/(1-e*np.cos(x))**4
 
 def t3(x,m,s,e):
-	return exp(i*s*(x-e*sin(x)))*sin(2*x)/(1-e*cos(u))**4
+	return np.exp(1j*s*(x-e*np.sin(x)))*np.sin(2*x)/(1-e*np.cos(x))**4
 
 def t4(x,m,s,e):
-	return exp(i*s*(x-e*sin(x)))*sin(x)/(1-e*cos(u))**4
+	return np.exp(1j*s*(x-e*np.sin(x)))*np.sin(x)/(1-e*np.cos(x))**4
 	
 # The overall coefficient thing
 def p_MS(m,s,e):
 	
-	p0s = quad(p_0s,0,2*pi,args=(m,s,e))
+	p0s = integrate.quad(p_0s,0,2*np.pi,args=(m,s,e))
 	
 	if abs(m) == 2:
 	
 		sC1 = 1-e**2    # Some constant
 		sC2 = sqrt(sC1) # Some other constant
 		
-		term1 = -sC1*quad(t1,0,2*pi,args=(m,s,e))
-		term2 = sC1*quad(t2,0,2*pi,args=(m,s,e))
-		term3 = i*sC2*quad(t3,0,2*pi,args=(m,s,e))
-		term4 = i*2*e*sC2*quad(t4,0,2*pi,args=(m,s,e))
+		term1 = -sC1*integrate.quad(t1,0,2*np.pi,args=(m,s,e))
+		term2 = sC1*integrate.quad(t2,0,2*np.pi,args=(m,s,e))
+		term3 = i*sC2*integrate.quad(t3,0,2*np.pi,args=(m,s,e))
+		term4 = i*2*e*sC2*integrate.quad(t4,0,2*np.pi,args=(m,s,e))
 		
 		if m > 0:
 			term3 = -term3
@@ -71,9 +71,9 @@ def p_MS(m,s,e):
 			term4 = -term4
 		
 	else:
-		term1=term2=term3=term4 = 0
+		term1=term2=term3=term4 = (0,0)
 		
-	return (1/(2*pi))*(p0s+term1+term2+term3+term4)
+	return (1/(2*np.pi))*(p0s[1]+term1[1]+term2[1]+term3[1]+term4[1])
 
 # Documentation goes here
 # Gets the specified coefficient of the (Chebyshev?) expansion
@@ -103,15 +103,16 @@ def getCoefficients(coeffDeg,eList,yList):
 
 # I'm not sure if I want the user to specify a range of m and s or just a specific p_ms (the latter would need
 # the program to be run individually for each p_ms coefficient)
-def main(m=0, s=1, accuracyGoal, maxCoeffs):
+def main(m=0, s=1, accuracyGoal=3, maxCoeffs=30):
 	
 	# Sanity check input arguments
 		# Correct number of them? Correct type? Acceptable values?
 		# If not, complain and exit
 		
 	# Initialize my variables
-	coeffDeg = [0:5] # Current degree(s) of Chebyshev polynomial for which we are finding coefficients
-	listOfCoeff = np.zeros(2,however many coefficients)
+	coeffDeg = 0#[0:5] # Current degree(s) of Chebyshev polynomial for which we are finding coefficients
+	listOfCoeff = np.zeros((2,1))
+	acc = 0
 	
 	# Optional?
 	# Determine range of e we need to deal with (for speed purposes)
@@ -120,28 +121,37 @@ def main(m=0, s=1, accuracyGoal, maxCoeffs):
 	
 	# Optional.
 	# Precalculate p_ms(e) itself to compare to? Can I do that? Would be convenient
-	
+	print("hi")
 	#Calculate some number of data points for 0<=e<=1
 	eList = np.zeros(GRID)
 	for i in range(1,GRID-1):
 		eList[i] = i*1/(GRID-1)
 	
 	# Calculate the value of p_ms for each point on that list
-	vec_pms = vecorize(p_MS) # Allow the bit I defined to take a vector for arguments, hassle-free
+	vec_pms = np.vectorize(p_MS) # Allow the bit I defined to take a vector for arguments, hassle-free
 	yList = vec_pms(m,s,eList)
 	
-	while (acc < accuracyGoal && length(listOfCoeff[1]) < maxCoeffs):
+	while (acc < accuracyGoal and coeffDeg <= maxCoeffs):
 		
-		newTerms = getCoefficients(termsToFind,eList,yList)
-		listOfCoeff = np.concatenate((listOfCoeff,np.array([termsToFind,newTerms])) )
+		newTerm = getCoefficients(coeffDeg,eList,yList)
 		
-		# Wait to do this one until you have something running so you can see if it's necessary
+		# if does not blow up (use prune, return true or false) # Wait to do this one until you have something running so you can see if it's necessary
+		#print(listOfCoeff)
+		#print(coeffDeg)
+		#print(newTerm[0])
+		#print((coeffDeg,newTerm[0]))
+		#print(np.array([[coeffDeg],[newTerm[0]]]))
+		listOfCoeff = np.concatenate((listOfCoeff,np.array([[coeffDeg],[newTerm[0]]])) )
+		
 		# ListOfCoefficients = prune(listOfC...) # In Mathematica, sometimes we got terms that exploded, but then ones after that
 											   #were fine, so I want to be able to remove problematic terms
 		
 		# accuracy = checkAccuracy(ListOfCoefficients, equation, [range of e], pms) # Check to see how accurate we are
+		
+		coeffDeg += 1
 	
 	# Report results
+	print(listOfCoeff)
 
 if __name__ == "__main__":
 	main()
