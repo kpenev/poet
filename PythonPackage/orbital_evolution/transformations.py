af3de8808d703342be20da16efa2eed11e6d7666
Kaloyan Penev <kpenev@gmail.com>
2021-02-25 10:22:40 -0600

    Add phase lag -> log10(Q) transformation

--- PythonPackage/orbital_evolution/transformations.py
+++ PythonPackage/orbital_evolution/transformations.py
@@ -1,6 +1,6 @@
 """For now just define transformation from log10(Q) to phase lag."""
 
-from math import pi
+from math import pi, log10
 
 #This is the most intuitive name for log10(Q).
 #pylint: disable=invalid-name
@@ -8,4 +8,9 @@ def phase_lag(lgQ):
     """Return the phase lag corresponding to the given Q value."""
 
     return 15.0 / (16.0 * pi * 10.0**lgQ)
+
+def lgQ(phase_lag):
+    """Return the log10(Q) value corresponding to the given phase lag."""
+
+    return log10(15.0 / (16.0 * pi * phase_lag))
 #pylint: enable=invalid-name
