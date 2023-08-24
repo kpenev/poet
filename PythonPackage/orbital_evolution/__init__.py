f5348f5d003d70c9eca2481ab056adb1c67894c2
Kaloyan Penev <kaloyan.penev@utdallas.edu>
2023-08-24 14:21:55 -0500

    Add easier access to some python clasess

--- PythonPackage/orbital_evolution/__init__.py
+++ PythonPackage/orbital_evolution/__init__.py
@@ -7,6 +7,8 @@ from orbital_evolution.star_interface import EvolvingStar
 from orbital_evolution.planet_interface import LockedPlanet
 from orbital_evolution.initial_condition_solver import InitialConditionSolver
 from orbital_evolution.transformations import phase_lag, lgQ
+from orbital_evolution.binary import Binary
+from orbital_evolution.learning_ic_solver import LearningICSolver
 
 __all__ = [os.path.basename(fname).rstrip('.py')
            for fname in glob(os.path.join(__path__[0], '*.py'))]
